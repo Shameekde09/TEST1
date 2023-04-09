1e66409c4fcfe5d507f0023cf97c9e2589904a55
Shameekde09 <128804894+Shameekde09@users.noreply.github.com>
2023-04-09 13:21:01 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,7 @@
+char=input("press any key: ")
+if(char.isalpha()):
+   print("The user has entered a charecter")
+if(char.isdigt()):
+   print("The user has entered a digit")
+if(char.isspace()):
+   print("The user has entered a white space charecter")
